94b1649f0b6478d55baaa4f5cd884c1e09f65866
Matias Hernandez <matiasfh@gmail.com>
2019-11-04 04:23:45 -0300

    add a mock api data for users

--- src/App.js
+++ src/App.js
@@ -1,26 +1,69 @@
 // @flow
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { ThemeProvider } from "emotion-theming";
+import styled from "@emotion/styled/macro";
+import { Global, css } from "@emotion/core";
+import emotionNormalize from "emotion-normalize";
+import Footer from "components/Footer";
+
+import Login from "screens/Login";
+const Home = React.lazy(() => import("screens/Home"));
+const Questions = React.lazy(() => import("screens/Questions"));
+
+const theme = {
+  colors: {
+    black: "#031323",
+    white: "#fff",
+    purple: "#AB61E5",
+    alabastro: "#F2F3F4",
+    blackPearl: "#D9DCDE"
+  }
+};
+
+const MainContainer = styled.main`
+  display: flex;
+  min-height: 100vh;
+  flex-direction: column;
+`;
+
+// This container is used as fallback to avoid flickering
+const Container = styled.div`
+  flex: 1;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Global
+        styles={css`
+          ${emotionNormalize}
+          html, body {
+            width: 100%;
+            height: 100%;
+            position: relative;
+            min-height: 100vh;
+          }
+        `}
+      />
+      <ThemeProvider theme={theme}>
+        <MainContainer>
+          <React.Suspense fallback={<Container />}>
+            <Router>
+              <Switch>
+                <Route exact path="/" component={Login} />
+                <Route path="/home" render={() => <Home />} />
+                <Route path="/questions" render={() => <Questions />} />
+              </Switch>
+            </Router>
+          </React.Suspense>
+          <Footer />
+        </MainContainer>
+      </ThemeProvider>
+    </>
   );
 }
 
